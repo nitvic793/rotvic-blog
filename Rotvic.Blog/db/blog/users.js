0d209676516a0c79933eb53ec711de2be9901294
nitvic793 <nithish_victor@yahoo.co.in>
2015-06-04 19:03:11 +0530

    Add more files

---
+++ Rotvic.Blog/db/blog/users.js
@@ -0,0 +1,14 @@
+﻿var mongojs = require('mongojs');
+var db = mongojs('rblog', ['posts', 'counters','users']);
+var utility = require('./../utility.js');
+
+exports.getUserName = function (id, callback) {
+    db.users.find({ 'id': id }, function (err, doc) {
+        if (!err) {
+            callback(doc[0].name);
+        }
+        else {
+            callback(null);
+        }
+    });
+}
